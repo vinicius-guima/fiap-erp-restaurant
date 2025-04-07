ba4dd5854f15f686c0da7ea2c8028a4b34393949
vinicius-guima <vinicius_guima.social@hotmail.com>
2025-04-06 23:47:31 -0300

    initial commit

---
+++ src/main/java/br/com/fiap/tech/challenge/erp_restaurant/ErpRestaurantApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech.challenge.erp_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/tech/challenge/erp_restaurant/ErpRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech.challenge.erp_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
