73cd71c426c2a1fdbf7c6cdb0b7418a01a2e29ab
Clebson Moura <clebson.sh@gmail.com>
2025-05-16 18:10:14 -0300

    refactor: replace hardcode status codes for HttpStatus enum

--- src/main/java/br/com/fiap/tech/challenge/erp_restaurant/infrastructure/controller/handler/ExceptionHandlerController.java
+++ src/main/java/br/com/fiap/tech/challenge/erp_restaurant/infrastructure/controller/handler/ExceptionHandlerController.java
@@ -3,15 +3,18 @@ package br.com.fiap.tech.challenge.erp_restaurant.infrastructure.controller.hand
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.fiap.tech.challenge.erp_restaurant.application.exception.AuthenticationException;
+import br.com.fiap.tech.challenge.erp_restaurant.application.exception.NotFoundException;
+import br.com.fiap.tech.challenge.erp_restaurant.application.exception.RoleException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
-import br.com.fiap.tech.challenge.erp_restaurant.application.exception.AuthenticationException;
-import br.com.fiap.tech.challenge.erp_restaurant.application.exception.NotFoundException;
-import br.com.fiap.tech.challenge.erp_restaurant.application.exception.RoleException;
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class ExceptionHandlerController {
@@ -45,4 +48,32 @@ public class ExceptionHandlerController {
 		}
 		return ResponseEntity.status(400).body(errors);	
 	}
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorMessage> handleNotFoundException(NoResourceFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorMessage("No signs of humanity here"));
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorMessage> handleNotFoundException(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorMessage(ex.getMessage()));
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorMessage> handleAuthenticationException(AuthenticationException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorMessage(ex.getMessage()));
+    }
+
+    @ExceptionHandler(RoleException.class)
+    public ResponseEntity<ErrorMessage> handleRoleException(RoleException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorMessage(ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handleNotFoundException(MethodArgumentNotValidException ex) {
+        List<String> errors = new ArrayList<>();
+        for (var error : ex.getBindingResult().getFieldErrors()) {
+            errors.add(error.getField() + " " + error.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
 }
